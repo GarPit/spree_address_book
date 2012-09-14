d80f3aff87da595efeeb768f18fd3165feef4868
PitOn <garik.piton@gmail.com>
2012-09-14 17:48:10 +0400

    remove Spree namespace

--- app/overrides/views_decorator.rb
+++ app/overrides/views_decorator.rb
@@ -1,6 +1,6 @@
 Deface::Override.new(
-  :virtual_path => "spree/users/show",
+  :virtual_path => "users/show",
   :name => "address_book_account_my_orders",
   :insert_after => "[data-hook='account_my_orders'], #account_my_orders[data-hook]",
-  :partial => "spree/users/addresses",
+  :partial => "users/addresses",
   :disabled => false)
