75d7bef554efb8ce301b061a83f3a2bedef9643d
PitOn <garik.piton@gmail.com>
2012-09-14 17:46:50 +0400

    remove Spree namespace

--- app/controllers/spree/addresses_controller.rb
+++ app/controllers/spree/addresses_controller.rb
@@ -1,5 +1,5 @@
-class Spree::AddressesController < Spree::BaseController
-  helper Spree::AddressesHelper
+class AddressesController < Spree::BaseController
+  helper AddressesHelper
   rescue_from ActiveRecord::RecordNotFound, :with => :render_404
   load_and_authorize_resource
   
