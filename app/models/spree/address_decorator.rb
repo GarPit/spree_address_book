9e51e6de38c530d6ea05adbee586c84015743ee5
PitOn <garik.piton@gmail.com>
2012-09-14 17:44:46 +0400

    remove Spree namespace

--- app/models/spree/address_decorator.rb
+++ app/models/spree/address_decorator.rb
@@ -1,20 +1,20 @@
-Spree::Address.class_eval do
+Address.class_eval do
   belongs_to :user
 
   attr_accessible :user_id, :deleted_at
 
   def self.required_fields
-    validator = Spree::Address.validators.find{|v| v.kind_of?(ActiveModel::Validations::PresenceValidator)}
+    validator = Address.validators.find{|v| v.kind_of?(ActiveModel::Validations::PresenceValidator)}
     validator ? validator.attributes : []
   end
   
   # can modify an address if it's not been used in an order 
   def editable?
-    new_record? || (shipments.empty? && (Spree::Order.where("bill_address_id = ?", self.id).count + Spree::Order.where("bill_address_id = ?", self.id).count <= 1) && Spree::Order.complete.where("bill_address_id = ? OR ship_address_id = ?", self.id, self.id).count == 0)
+    new_record? || (shipments.empty? && (Order.where("bill_address_id = ?", self.id).count + Order.where("bill_address_id = ?", self.id).count <= 1) && Order.complete.where("bill_address_id = ? OR ship_address_id = ?", self.id, self.id).count == 0)
   end
   
   def can_be_deleted?
-    shipments.empty? && Spree::Order.where("bill_address_id = ? OR ship_address_id = ?", self.id, self.id).count == 0
+    shipments.empty? && Order.where("bill_address_id = ? OR ship_address_id = ?", self.id, self.id).count == 0
   end
   
   def to_s
