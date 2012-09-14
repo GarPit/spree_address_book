820d36e78c758fad97da1d341c27fa3e1b781396
PitOn <garik.piton@gmail.com>
2012-09-14 17:56:22 +0400

    remove Spree namespace

--- lib/spree_address_book.rb
+++ lib/spree_address_book.rb
@@ -6,16 +6,16 @@ module Spree
       engine_name 'spree_address_book'
       
       initializer "spree.advanced_cart.environment", :before => :load_config_initializers do |app|
-        Spree::AddressBook::Config = Spree::AddressBookConfiguration.new
+        Spree::AddressBook::Config = AddressBookConfiguration.new
       end
       
       config.autoload_paths += %W(#{config.root}/lib)
 
       def self.activate
-        Dir.glob(File.join(File.dirname(__FILE__), "../app/**/spree/*_decorator*.rb")) do |c|
+        Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator*.rb")) do |c|
           Rails.application.config.cache_classes ? require(c) : load(c)
         end
-        Spree::Ability.register_ability(Spree::AddressAbility)
+        Ability.register_ability(AddressAbility)
       end
 
       config.to_prepare &method(:activate).to_proc
